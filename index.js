ac24289231a7e4b710bae64865e00ed878ffa6f0
Fuyu <reuye8@gmail.com>
2026-01-11 12:24:01 +0900

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  async fetch(req, env) {
-    const html = await fetch("https://example.com/tablet.html").then(r => r.text());
-    return new Response(html, { headers: { "Content-Type": "text/html" } });
-  }
-}
